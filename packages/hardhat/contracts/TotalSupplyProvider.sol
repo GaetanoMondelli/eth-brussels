ee2ed22de0be26d288f64e3db8334ba0276dd84a
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-07-13 15:12:34 +0100

    fix: supply provider to contain also the name of token in tags and label

--- packages/hardhat/contracts/TotalSupplyProvider.sol
+++ packages/hardhat/contracts/TotalSupplyProvider.sol
@@ -5,28 +5,35 @@ import "./IDataProvider.sol";
 import { IERC20 } from "./IERC20.sol";
 
 contract TotalSupplyProvider is IDataProvider {
-    IERC20 public token;
+	IERC20 public token;
 
-    constructor(IERC20 _token) {
-        token = _token;
-    }
+	constructor(IERC20 _token) {
+		token = _token;
+	}
 
-    function getLabel() external view override returns (string memory) {
-        return "Total Supply";
-    }
+	function getLabel() external view override returns (string memory) {
+		return
+			string(
+				abi.encodePacked("totalSupply", IERC20(address(token)).name())
+			);
+	}
 
-    function getMetricData(address tokenA) external view override returns (uint256) {
-        require(tokenA == address(token), "INVALID_TOKEN");
-        return token.totalSupply();
-    }
+	function getMetricData(
+		address tokenA
+	) external view override returns (uint256) {
+		require(tokenA == address(token), "INVALID_TOKEN");
+		return token.totalSupply();
+	}
 
-    function getTags() external view override returns (string[] memory) {
-        string[] memory tags = new string[](1);
-        tags[0] = "total-supply";
-        return tags;
-    }
+	function getTags() external view override returns (string[] memory) {
+		string[] memory tags = new string[](4);
+		tags[0] = "onchain";
+		tags[1] = "supply";
+		tags[2] = IERC20(address(token)).name();
+		return tags;
+	}
 
-    function getDataTimestamp() external view override returns (uint256) {
-        return block.timestamp;
-    }
+	function getDataTimestamp() external view override returns (uint256) {
+		return block.timestamp;
+	}
 }
