b238757ca3299111d14e7d153439b2ba0f89b30c
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-07-13 15:04:16 +0100

    feat: added uniswap liquidity provider

--- packages/hardhat/contracts/UniswapV3LiquidityProvider.sol
+++ packages/hardhat/contracts/UniswapV3LiquidityProvider.sol
@@ -4,15 +4,19 @@ pragma solidity ^0.8.0;
 import "./IUniswapV3Factory.sol";
 import "./IUniswapV3Pool.sol";
 import "./ILiquidityProvider.sol";
+import "./IDataProvider.sol";
 
-contract UniswapV3LiquidityProvider is ILiquidityProvider {
+contract UniswapV3LiquidityProvider is ILiquidityProvider, IDataProvider {
 	IUniswapV3Factory public factory;
 	uint24[] public feeTiers = [500, 3000, 10000]; // Example fee tiers: 0.05%, 0.3%, 1% these are Uniswap V3 standards
 	address[] public comparisonTokens;
+	uint256 public mockPrice = 1000;
+	string tokenName;
 
-	constructor(address _factory, address[] memory _comparisonTokens) {
+	constructor(address _factory, address[] memory _comparisonTokens, string memory _tokenName) {
 		factory = IUniswapV3Factory(_factory);
 		comparisonTokens = _comparisonTokens;
+		tokenName = _tokenName;
 	}
 
 	function getPoolsForToken(
@@ -55,5 +59,42 @@ contract UniswapV3LiquidityProvider is ILiquidityProvider {
 			totalLiquidity += 1;
 			IUniswapV3Pool(pools[i]).liquidity();
 		}
+	}
+
+	function getPrice(
+		address token
+	) public view returns (uint256 averagePrice) {
+		uint256 sumPrice = 0;
+		uint256 count = 0;
+		address[] memory pools = getPoolsForToken(token);
+
+		for (uint256 i = 0; i < pools.length; i++) {
+			sumPrice += mockPrice;
+			count++;
+		}
+
+		if (count > 0) {
+			averagePrice = sumPrice / count;
+		}
+	}
+
+	function getLabel() external view override returns (string memory) {
+		return string(abi.encodePacked("UniswapV3LiquidityProvider", tokenName));
+	}
+
+	function getMetricData(
+		address tokenA
+	) external view override returns (uint256) {
+		return getTokenLiquidity(tokenA);
+	}
+
+	function getTags() external view override returns (string[] memory) {
+		// return "onchain", "uniswap", "liquidity";
+		string[] memory tags = new string[](4);
+		tags[0] = "onchain";
+		tags[1] = "uniswap";
+		tags[2] = "liquidity";
+		tags[3] = tokenName;
+		return tags;
 	}
 }
---
+++ packages/hardhat/contracts/UniswapV4LiquidityProvider.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.20;
+
+import { FixedLiquidityPriceOracle, Oracle } from "./FixedLiquidityPriceOracle.sol";
+import "./ILiquidityProvider.sol";
+import { PoolId, PoolKey } from "./Uniswap/V4-Core/types/PoolId.sol";
+import { TickMath } from "./Uniswap/V4-Core/libraries/TickMath.sol";
+import "./IDataProvider.sol";
+import { IERC20 } from "./IERC20.sol";
+
+contract UniswapV4LiquidityProvider is ILiquidityProvider, IDataProvider {
+	FixedLiquidityPriceOracle public v4HookPriceOracle;
+	PoolKey[] public poolKeysForSameAsset;
+	address token;
+
+	constructor(
+		address _v4HookPriceOracle,
+		PoolKey[] memory _poolKeysForSameAsset,
+		address _token
+	) {
+		v4HookPriceOracle = FixedLiquidityPriceOracle(_v4HookPriceOracle);
+		for (uint256 i = 0; i < _poolKeysForSameAsset.length; i++) {
+			poolKeysForSameAsset.push(_poolKeysForSameAsset[i]);
+		}
+		token = _token; //same as currency0
+	}
+
+	function getTokenLiquidity(
+		address _token
+	) public view returns (uint256 totalLiquidity) {
+		require(_token == token, "Invalid token address");
+		uint256 sumLiquidity = 0;
+		uint256 count = 0;
+
+		for (uint256 i = 0; i < poolKeysForSameAsset.length; i++) {
+			PoolKey memory key = poolKeysForSameAsset[i];
+			sumLiquidity += v4HookPriceOracle.getObservationLiquidity(key, 0);
+			count++;
+		}
+
+		if (count > 0) {
+			totalLiquidity = sumLiquidity / count;
+		}
+	}
+
+	function getPrice(
+		address token
+	) public view returns (uint256 averagePrice) {
+		int256 sumTickCumulative = 0;
+		uint256 count = 0;
+		uint32 startTime;
+		uint32 endTime;
+
+		for (uint256 i = 0; i < poolKeysForSameAsset.length; i++) {
+			PoolKey memory key = poolKeysForSameAsset[i];
+			PoolId poolId = PoolId.wrap(keccak256(abi.encode(key)));
+			FixedLiquidityPriceOracle.ObservationState
+				memory state = v4HookPriceOracle.getState(key);
+
+			if (state.cardinality > 1) {
+				Oracle.Observation memory startObservation = v4HookPriceOracle
+					.getObservation(key, 0);
+				Oracle.Observation memory endObservation = v4HookPriceOracle
+					.getObservation(key, state.cardinality - 1);
+
+				startTime = startObservation.blockTimestamp;
+				endTime = endObservation.blockTimestamp;
+
+				sumTickCumulative +=
+					int256(endObservation.tickCumulative) -
+					int256(startObservation.tickCumulative);
+				count += (endTime - startTime);
+			}
+		}
+
+		if (count > 0) {
+			int256 averageTick = sumTickCumulative / int256(count);
+			averagePrice = tickToPrice(int24(averageTick));
+		}
+		return averagePrice;
+	}
+
+	function tickToPrice(int24 tick) internal pure returns (uint256 price) {
+		return TickMath.getSqrtRatioAtTick(int24(tick));
+	}
+
+	function getLabel() external view override returns (string memory) {
+		return string(abi.encodePacked("UniswapV4LiquidityProvider", IERC20(token).name()));
+	}
+
+	function getMetricData(
+		address tokenA
+	) external view override returns (uint256) {
+		return getTokenLiquidity(tokenA);
+	}
+
+	function getTags() external view override returns (string[] memory) {
+		string[] memory tags = new string[](4);
+		tags[0] = "onchain";
+		tags[1] = "uniswapv4";
+		tags[2] = "liquidity";
+		tags[3] = IERC20(token).name();
+		return tags;
+	}
+}
