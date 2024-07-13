a03f6bebe44a379cdf5240dddfdce793bc835ad4
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-07-13 01:03:30 +0100

    fix: adding working ompiler for hardhat hook

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -33,8 +33,35 @@ const config: HardhatUserConfig = {
     //   },
     // },
     compilers: [
+      // {
+      //   version: "0.8.24",
+      //   settings: {
+      //     optimizer: {
+      //       enabled: true,
+      //       runs: 200,
+      //     },
+      //   },
+      // },
       {
         version: "0.8.22",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.19",
         settings: {
           optimizer: {
             enabled: true,
